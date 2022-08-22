30964cccd37d3e3f31e41ed7efdefd4012c8544f
Thiago Garin <thggarin@gmail.com>
2022-08-22 19:21:43 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Pad from './components/Pad';
 import './App.css';
+import sounds from './sounds';
 
-function App() {
+const App = () => {
+
+  const [display, setDisplay] = useState("");
+
+  const drumSounds = sounds.map(sound => 
+    <Pad sound={sound} key={sound.id} setDisplay={setDisplay}/>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-dark min-vh-100 text-white text-center p-4">
+      <div id="drum-machine">
+        <h1 className="text-warning">Drum Machine</h1>
+        <div id="display" className="fs-3 m-4">
+          <h2 className="p-2">Key pressed</h2>
+          {display}
+        </div>
+          {drumSounds}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Pad.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+const Pad = ({ sound, setDisplay }) => {
+
+  const [select, setSelect] = useState(false)
+
+  useEffect(()=>{
+    document.addEventListener("keydown", handleKey)
+    return () => {
+      document.removeEventListener("keydown", handleKey)
+    }
+  }, [])
+
+  const handleKey = event => {
+    if(event.keyCode === sound.keyCode) {
+      playAudio()
+    }
+  }
+
+  const playAudio = () => {
+    const audio = document.getElementById(sound.keyTrigger)
+    console.log(audio)
+    setSelect(true);
+    setTimeout(()=> setSelect(false), 1500)
+    audio.currentTime = 0;
+    audio.play();
+    setDisplay(sound.keyTrigger)
+  }
+
+  return (
+      <div className={`drum-pad btn btn-secondary p-5 m-3 fs-4 ${ select && 'btn-warning'}`} id={sound.id} onClick={playAudio}>
+        {sound.keyTrigger}
+        <audio src={sound.url} className="clip" id={sound.keyTrigger}/>
+      </div>
+  )
+}
+
+export default Pad;
