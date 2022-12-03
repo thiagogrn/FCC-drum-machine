5e463b288435c82257706623adc69c4424345c56
Thiago Garin <104943480+thiagogrn@users.noreply.github.com>
2022-12-03 13:19:03 -0300

    docs: update Pad.js

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -2,29 +2,28 @@ import React, { useEffect, useState } from 'react';
 
 const Pad = ({ sound, setDisplay }) => {
 
-  const [select, setSelect] = useState(false)
+  const [select, setSelect] = useState(false);
 
   useEffect(()=>{
-    document.addEventListener("keydown", handleKey)
+    document.addEventListener("keydown", handleKey);
     return () => {
-      document.removeEventListener("keydown", handleKey)
+      document.removeEventListener("keydown", handleKey);
     }
   }, [])
 
   const handleKey = event => {
     if(event.keyCode === sound.keyCode) {
-      playAudio()
+      playAudio();
     }
   }
 
   const playAudio = () => {
-    const audio = document.getElementById(sound.keyTrigger)
-    console.log(audio)
+    const audio = document.getElementById(sound.keyTrigger);
     setSelect(true);
-    setTimeout(()=> setSelect(false), 1500)
+    setTimeout(()=> setSelect(false), 1500);
     audio.currentTime = 0;
     audio.play();
-    setDisplay(sound.keyTrigger)
+    setDisplay(sound.keyTrigger);
   }
 
   return (
